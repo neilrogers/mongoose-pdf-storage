34fc40beee2fdb3cd8ee679adb74aee8c4171f97
Neil Rogers <neil@jetconvert.com.au>
2016-01-12 13:24:30 +1000

    comments tidy up

--- client.js
+++ client.js
@@ -4,25 +4,32 @@ var request = require("request"),
 require('dotenv').config({path: __dirname+'/.env'});
 
 var file = fs.readFileSync("example.pdf");
-//file = new Buffer(file,'binary');
-//file = file.toString('binary');
 
 fs.writeFileSync("example1.pdf",file,'binary');
 
-//console.log(file);
-
+//POST the file to the server
 var options = { uri : "http://localhost:"+process.env.API_PORT+'/file', method : 'POST', headers: { 'Content-Type': 'text/plain', 'Content-Length': file.length }, body: file };
 requestPromise(options).then( function (result) {
+
 	result = JSON.parse(result);
 	return result;
+
 }).then( function (result) {
+
+	//GET the file from the server
 	options = { uri : "http://localhost:"+process.env.API_PORT+'/file/'+result._id, encoding: 'binary',method : 'GET' };
 	return requestPromise(options);
+
 }).then( function (result) {
+
+	//Write the contents to a file
 	console.log(result);
 	fs.writeFileSync("example2.pdf",result,'binary');
+
 }).catch(function (error) {
+
 	console.log(error);
+
 });
 
 function requestPromise(options) {
